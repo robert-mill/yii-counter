c913683e56d7eb7f4d1802dddef048763c921df6
Robert <r.mill@ntlworld.com>
2017-11-09 17:15:32 +0000

    initial commit

---
+++ src/classes/CounterClass.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmill_000
+ * Date: 09/11/2017
+ * Time: 17:14
+ */
+
+class CounterClass
+{
+    public function __construct()
+    {
+        return 'hi';
+    }
+}
